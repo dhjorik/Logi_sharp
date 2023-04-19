8917ebbcea0e1f453be44c1c5524d141d81478e0
Matteo <mboffadossi@marvell.com>
2023-04-19 14:43:55 +0200

    [2023-04-19] Added data and util files

---
+++ LogiWorld/LogiSim/data/Attribute.cs
@@ -0,0 +1,33 @@
+﻿using LogiSim.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiSim
+{
+    public abstract class Attribute<V>
+    {
+        private string name;
+        private StringGetter disp;
+
+        public Attribute(String name, StringGetter disp)
+        {
+            this.name = name;
+            this.disp = disp;
+        }
+
+        public override string ToString()
+        {
+            return this.name;
+        }
+
+        public string GetName() { return this.name; }
+
+        public string GetDisplayName() { return this.disp.Get; }
+
+        public string ToDisplayString(V value) { return value == null ? "" : value.ToString(); }
+        public string ToStandardString(V value) { return value.ToString(); }
+    }
+}
